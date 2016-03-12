9be989877a68385f1411253ea27c85d0dde5f8ba
sudhakarCS5542 <p.reddy.sudhakar@gmail.com>
2016-03-11 23:42:42 -0600

    Uploading Increment 2 project phase.

--- src/Robo/mobile/src/main/java/com/example/sudhakareddy/robo/MainActivity.java
+++ src/Robo/mobile/src/main/java/com/example/sudhakareddy/robo/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.sudhakareddy.robo;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -47,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
                 File file = getFile();
                 camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
                 startActivityForResult(camera_intent, CAM_REQUEST);
+                OCRAction(file);
             }
         });
         getData.setOnClickListener(new View.OnClickListener() {
@@ -58,6 +64,79 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //OCR activity - modify image to remove skewness and apply Tesseract
+    private void OCRAction(File file){
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 4;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(file.getPath(), options);
+
+        try {
+            ExifInterface exif = new ExifInterface(file.getPath());
+            int exifOrientation = exif.getAttributeInt(
+                    ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_NORMAL);
+
+            int rotate = 0;
+            switch (exifOrientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    rotate = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    rotate = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    rotate = 270;
+                    break;
+            }
+
+            if (rotate != 0) {
+
+                // Getting width & height of the given image.
+                int w = bitmap.getWidth();
+                int h = bitmap.getHeight();
+
+                // Setting pre rotate
+                Matrix mtx = new Matrix();
+                mtx.preRotate(rotate);
+
+                // Rotating Bitmap
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
+            }
+
+            // Convert to ARGB_8888, required by tess
+            bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+
+        } catch (IOException e) {
+        }
+
+        //Process Bitmap by OCR
+        TessBaseAPI baseApi = new TessBaseAPI();
+        baseApi.setDebug(true);
+        baseApi.init(DATA_PATH, lang);
+        baseApi.setImage(bitmap);
+
+        String recognizedText = baseApi.getUTF8Text();
+
+        baseApi.end();
+
+        // Text in recognizedText var, you can do anything with it.
+        // We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
+        // so that garbage doesn't make it to the display.
+
+        if ( lang.equalsIgnoreCase("eng") ) {
+            recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
+        }
+
+        recognizedText = recognizedText.trim();
+
+        if ( recognizedText.length() != 0 ) {
+            _field.setText(_field.getText().toString().length() == 0 ? recognizedText : _field.getText() + " " + recognizedText);
+            _field.setSelection(_field.getText().toString().length());
+        }
+
+    }
+    
     private File getFile(){
         File imageFolder = new File("sdcard/image_storage");
 
@@ -66,7 +145,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         File image_file = new File(imageFolder, "image.jpg");
-        return null;
+        return image_file;
     }
 
     @Override
